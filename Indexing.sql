b5233e22b189b8d9b0d96786d1d0cc37c3b9e2c2
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2022-05-08 20:54:47 +0100

    indexing, falta 7 e 8

--- Indexing.sql
+++ Indexing.sql
@@ -1,3 +1,4 @@
+-- executation plan for the query
 explain plan set statement_id='q1' into plan_table for
 select * from uscensus where id=9000;
 
@@ -24,9 +25,63 @@ add constraint pk_uscensus primary key(id);
 ALTER TABLE USCENSUS
 DROP PRIMARY KEY;
 
--- force index/no index(not working)
+
 select  /*+ NO_INDEX (uscensus pk_uscensus)*/ * from uscensus where id=9000;
 
--- 3
+-- 4 B+ Tree
+
+select AVG(age) from uscensus where education = 'Preschool';
+
+-- create index over education
+create index education_index on uscensus (education);
+
+-- 70287 ms without index, 4023 with index
+select sql_text, elapsed_time
+from V$SQLAREA
+where sql_text like 'select AVG(age) from uscensus where education = ''Preschool''';
+
+-- drops index
+drop index education_index;
+
+-- 5 bitmap index
+
+select AVG(age) from uscensus where education = 'Preschool';
+
+create bitmap index education_index on uscensus (education);
+
+-- 4026 with bitmap index
+select sql_text, elapsed_time
+from V$SQLAREA
+where sql_text like 'select AVG(age) from uscensus where education = ''Preschool''';
 
 
+-- 6
+
+select COUNT(*) from uscensus where education = 'Preschool';
+
+-- 10958 ms without index, 3518 with B+Tree, 3670 with bitmap
+select sql_text, elapsed_time
+from V$SQLAREA
+where sql_text like 'select COUNT(*) from uscensus where education = ''Preschool''';
+
+-- 7
+
+select * from uscensus where sex = 'Female' and country = 'Scotland' and income = '>50K';
+
+-- 11118 ms without index, 
+select sql_text, elapsed_time
+from V$SQLAREA
+where sql_text like 'select * from uscensus where sex = ''Female'' and country = ''Scotland'' and income = ''>50K''';
+
+--Indices for sex, country, income, (sex, country), (country, sex), (sex, income), (income, sex), (country, income), (income, sex)
+-- (sex, country, income), (sex, income, country), (country, sex, income), (country, income, sex), (income, sex, country) and (income, country, sex)
+
+
+
+
+--8
+
+explain plan set statement_id='q1' into plan_table for
+-- selects to test
+
+select operation, options, object_name, position, cost from plan_table; -- COST = 2, 2, 1
