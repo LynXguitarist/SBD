092d8367576d27c39569eef062dac519fb1900e5
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2022-06-14 19:06:35 +0100

    inswxing

---
+++ APRESENTAÇÃO/SQL SERVER/INDEXING.sql
@@ -0,0 +1,6 @@
+-- 1 -> comparar todos os indices iguais no Oracle vs SQL Server
+	-- Clustered, NonClustered, hash
+
+-- 2 -> ver tambem indices diferentes
+	-- SQL Server -> 1computed columns, 2filtered (parecido ao bitmap), full text, columnstore
+	-- Oracle -> 1Index Based on Arithmetic Expression, 2bitmap
