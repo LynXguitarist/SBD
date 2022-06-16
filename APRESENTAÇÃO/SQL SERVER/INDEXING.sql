7a2992f75294de627a303990d8bd2e2e9c97f6b3
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2022-06-16 22:36:25 +0100

    indexing and two new DBs

--- APRESENTAÇÃO/SQL SERVER/INDEXING.sql
+++ APRESENTAÇÃO/SQL SERVER/INDEXING.sql
@@ -1,6 +1,13 @@
 -- 1 -> comparar todos os indices iguais no Oracle vs SQL Server
-	-- Clustered, NonClustered, hash
+	-- Clustered, NonClustered
 
 -- 2 -> ver tambem indices diferentes
-	-- SQL Server -> 1computed columns, 2filtered (parecido ao bitmap), full text, columnstore
-	-- Oracle -> 1Index Based on Arithmetic Expression, 2bitmap
+	-- SQL Server -> 2filtered (parecido ao bitmap), full text
+	-- Oracle -> 2bitmap
+
+-- 3 -> Ver hash, comparar Oracle vs SQL Server e dps passar para Simões testar
+	-- se mais rápido index + hash join ou só um deles
+
+-- 4 -> mostrar included columns vs multi attribute index
+
+select * from alunos;
