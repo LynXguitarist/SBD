77eca33fc3f0131b59b034db0e7d6170bd8f508d
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2022-06-18 18:50:12 +0100

    new DB

---
+++ APRESENTAÇÃO/INDEXING.sql
@@ -0,0 +1,7 @@
+-- included columns vs multi attribute index
+-- filtered, full text
+-- Ver hash, comparar Oracle vs SQL Server e dps passar para Simões testar
+	-- se mais rápido index + hash join ou só um deles
+
+
+select * from alunos;
